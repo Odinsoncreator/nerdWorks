131c8cc09ad03700e48bf8a7ad09a20c53d58e4d
Antilus H <111935651+Odinsoncreator@users.noreply.github.com>
2023-05-31 20:48:14 -0600

    Tienda.java

--- Tienda.java
+++ Tienda.java
@@ -18,6 +18,7 @@ public class Tienda {
     }
 
     public void cargarProductos(String ruta) {
+        // Carga los productos desde los archivos de texto en la ruta especificada
         File directorio = new File(ruta);
         File[] archivos = directorio.listFiles();
         productos = new ArrayList<>();
@@ -32,6 +33,7 @@ public class Tienda {
     }
 
     private void leerArchivo(File archivo) {
+        // Lee un archivo de productos y crea objetos Producto para cada línea del archivo
         try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
             String linea;
             while ((linea = br.readLine()) != null) {
@@ -49,6 +51,7 @@ public class Tienda {
     }
 
     public void mostrarProductos() {
+        // Muestra los productos disponibles en la tienda
         System.out.println("                                            ");
         System.out.println("*==*==*==*==*== PRODUCTOS ==*==*==*==*==*");
         System.out.println(" ");
@@ -58,14 +61,18 @@ public class Tienda {
         }
         System.out.println(" ");
         System.out.println("==========================================");
+        System.out.println("Ingrese el número de producto que desea seleccionar PRESIONE 0 para finalizar ");
+        System.out.println("Ingrese la cantidad de producto que desea adquirir ");
+        System.out.println(" ");
     }
 
     public void seleccionarProductos() {
+        // Permite al usuario seleccionar productos y cantidades para comprar
         productosSeleccionados = new HashMap<>();
         precioTotal = 0;
 
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Ingrese el número de producto que desea seleccionar (0 para finalizar):");
+            System.out.println("Selecciona el producto: ");
 
             int opcion = scanner.nextInt();
             scanner.nextLine(); // Limpia el búfer del scanner
@@ -76,7 +83,7 @@ public class Tienda {
                 } else {
                     Producto productoSeleccionado = productos.get(opcion - 1);
 
-                    System.out.print("Ingrese la cantidad deseada: ");
+                    System.out.print("Ingrese la cantidad de producto: ");
                     int cantidad = scanner.nextInt();
                     scanner.nextLine(); // Limpia el búfer del scanner
 
@@ -84,7 +91,7 @@ public class Tienda {
                     precioTotal += productoSeleccionado.getPrecio() * cantidad;
                 }
 
-                System.out.println("Ingrese el número de producto que desea seleccionar (0 para finalizar):");
+                System.out.println("Selecciona el producto:");
                 opcion = scanner.nextInt();
                 scanner.nextLine(); // Limpia el búfer del scanner
             }
@@ -92,8 +99,32 @@ public class Tienda {
             if (productosSeleccionados.isEmpty()) {
                 System.out.println("No se ha seleccionado ningún producto.");
             } else {
-                Factura factura = new Factura(productosSeleccionados, usuarioActual, precioTotal);
-                factura.imprimirFactura();
+                System.out.println(" ");
+                System.out.println("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/* ");
+                System.out.println(" ");
+                System.out.println("Verifique el nombre de usuario para generar su factura: ");
+                String nombreUsuario = scanner.nextLine();
+
+                String rutaUsuario = "INGRESA LA RUTA DE LA CARPETA  QUE ALMACENARA TUS USUARIOS" + nombreUsuario + ".txt";
+                File archivoUsuario = new File(rutaUsuario);
+
+                if (archivoUsuario.exists()) {
+                    usuarioActual = new Usuario(nombreUsuario, "", "");
+                    usuarioActual.setArchivo(rutaUsuario);
+
+                    Factura factura = new Factura(productosSeleccionados, usuarioActual, precioTotal);
+                    System.out.println(" ");
+                    System.out.println("/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*/*  ");
+                    System.out.println(" ");
+                    System.out.println("GENERANDO FACTURA ... ");
+                    System.out.println(" ");
+                    System.out.println("IMPRIMIENDO...");
+                    System.out.print(" ");
+                    factura.imprimirFactura();
+                } else {
+                    System.out.println("El nombre de usuario no existe. No se puede generar la factura.");
+                }
+                System.exit(0);
             }
         }
     }
