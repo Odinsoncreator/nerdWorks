04af24b4e4ec9acf37e6044e86633712c0d43986
Antilus H <111935651+Odinsoncreator@users.noreply.github.com>
2023-05-31 20:50:00 -0600

    Factura.java

--- Factura.java
+++ Factura.java
@@ -1,15 +1,19 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
+
 
 public class Factura {
     private Map<Producto, Integer> productosComprados;
-    private Usuario usuarioCompra;
     private double precioTotal;
     private double iva;
+    private Usuario usuarioCompra;
 
     public Factura(Map<Producto, Integer> productosComprados, Usuario usuarioCompra, double precioTotal) {
+        // Inicialización de los atributos de la factura con los valores proporcionados
         this.productosComprados = productosComprados;
         this.usuarioCompra = usuarioCompra;
         this.precioTotal = precioTotal;
@@ -17,41 +21,57 @@ public class Factura {
     }
 
     public void imprimirFactura() {
-        System.out.println("=========================================================================");
+        // Obtiene la fecha y hora actual
+        Date fechaHoraActual = new Date();
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        String fechaHoraImpresion = formatoFecha.format(fechaHoraActual);
+
+        // Imprime el encabezado de la factura
+        System.out.println("|======================================================================|");
         System.out.println("|                           FACTURA                                     |");
-        System.out.println("=========================================================================");
+        System.out.println("========================================================================|");
+        System.out.println("| Fecha de impresión: " + fechaHoraImpresion + "                               |");
+        System.out.println("|-----------------------------------------------------------------------|");
         System.out.println("| Producto                                      | Cantidad | Precio     |");
         System.out.println("|-----------------------------------------------+----------+------------|");
-    
+
+        // Itera sobre los productos comprados y los imprime en la factura
         for (Map.Entry<Producto, Integer> entry : productosComprados.entrySet()) {
             Producto producto = entry.getKey();
             int cantidad = entry.getValue();
-    
+
             String nombreProducto = producto.getNombre();
             String precio = String.format("%.2f", producto.getPrecio());
-    
+
             System.out.printf("| %-45s | %-8s | %-10s |%n", nombreProducto, cantidad, precio);
         }
-    
+
+        // Imprime la sección de totales
         System.out.println("|-----------------------------------------------+----------+------------|");
         String subtotal = String.format("%.1f", precioTotal);
         String ivaAmount = String.format("%.1f", iva);
         String totalConIVA = String.format("%.1f", (precioTotal + iva));
-    
+
         System.out.printf("| Total (sin IVA)                               |          | %-11s|%n", subtotal);
         System.out.printf("| IVA                                           |          | %-11s|%n", ivaAmount);
         System.out.println("=========================================================================");
         System.out.printf("| Total (con IVA)                               |          | %-11s|%n", totalConIVA);
         System.out.println("=========================================================================");
+
+        // Imprime los datos del usuario desde su archivo
         System.out.println("| Datos del usuario:                                                    |");
+        System.out.println("|-----------------------------------------------------------------------|");
+
         try (BufferedReader br = new BufferedReader(new FileReader(usuarioCompra.getArchivo()))) {
             String linea;
             while ((linea = br.readLine()) != null) {
-                System.out.println(linea);
+                System.out.printf("| %-69s |%n", linea);
             }
         } catch (IOException e) {
-            System.out.println("Error al leer el archivo del usuario " + usuarioCompra.getNombre());
             e.printStackTrace();
         }
+
+        // Imprime el final de la factura
+        System.out.println("=========================================================================");
     }
 }
